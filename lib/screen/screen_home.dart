61d66d49cf4ce4c0ff5eb638fe1fb5d7367b7ac8
koabc9999 <koabc9999@gmail.com>
2021-06-28 16:55:24 +0900

    home, question screen demo

--- lib/screen/screen_home.dart
+++ lib/screen/screen_home.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
-
+import 'package:the_quiz/model/model_quiz.dart';
+import 'package:the_quiz/screen/screen_question.dart';
 
 class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Quiz quiz = Quiz.fromMap({// map 형식의 데이터를 받아서 Quiz타입 변수에 넣어주는 부분
+    'index': 1,
+    'title': '스위치 - 전구',
+    'question': '스위치 3개가 다른 방의 전구 3개와 각각 연결되어 있다.\n 스위치는 ON/OFF 조작을 자유롭게 할 수 있으며 초기에는 꺼져있다.\n 단 한 번만 다른 방으로 이동해서 전구의 상태를 확인할 수 있다고 할 때 각각의 스위치가 연결된 전구를 알아낼 수 있는 방법을 구하시오.',
+    'solution': '정답이 들어가는 부분입니다.',
+    'up': 0,
+    'down': 0
+  });
+
+
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
     double height = screenSize.height;// 활용할 화면 크기를 담은 변수
@@ -20,6 +31,9 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         body: Column(
           children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(top: height * 0.07),// .all 메소드를 사용하면 4방향에 여백이 넣어져서 여백이 2배가 됨
+            ),
             Center(
               child: Image.asset(
                 'images/pepe.jpg',// 픽셀이 깨지는걸 방지해서 본래 크기보다 이미지가 커지지는 않고 차지하는 범위가 늘어남
@@ -27,6 +41,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 height: height * 0.4,
               ),
             ),
+            Padding(padding: EdgeInsets.only(top: height * 0.02)),
             Text(
               '퀴즈 앱 홈',
               style: TextStyle(
@@ -34,16 +49,33 @@ class _HomeScreenState extends State<HomeScreen> {
                 fontWeight: FontWeight.bold,
               ),
             ),
+            Padding(padding: EdgeInsets.only(top: height * 0.03)),
             ElevatedButton(// RaisedButton에서 ElevatedButton으로 넘어옴
+              child: Text(
+                '문제  풀기',
+                style: TextStyle(
+                  fontSize: width * 0.06,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
               style: ElevatedButton.styleFrom(// 새로운 버튼 위젯의 스타일 사용방법
                 primary: Colors.amber[600],
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                padding: EdgeInsets.fromLTRB(width * 0.22, height * 0.03, width * 0.22, height * 0.03),
               ),
-              
-              onPressed: () => {},
-              child: Text('문제 풀기'),
+              onPressed: () {// 문제 풀기 버튼이 눌렸으 때 실행되는 내용
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => QuestionScreen(
+                      quiz: quiz,
+                    ),
+                  ),
+                );
+              },
             ),
-          ]
-        )
+          ],
+        ),
       ),
     );
   }
---
+++ lib/screen/screen_question.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:the_quiz/model/model_quiz.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+
+class QuestionScreen extends StatefulWidget {
+  Quiz quiz;
+  QuestionScreen({this.quiz});// 해당 클래스를 선언할 때 넣어준 첫번째 인자를 this.quiz에 넣어줌
+
+  _QuestionScreenState createState() => _QuestionScreenState();
+}
+
+class _QuestionScreenState extends State<QuestionScreen> {
+
+  Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    double width = screenSize.width;
+    double height = screenSize.height;
+
+    return SafeArea(
+      child: Scaffold(// 안에 다수의 위젯이 쌓일 수 있게 해주는 위젯
+        appBar: AppBar(
+          title: Text('무작위 문제'),
+          backgroundColor: Colors.amber[600],
+        ),
+        body: Center(// 위젯이 중심으로 가도록함
+          child: Container(
+            decoration: BoxDecoration(// 컨테이너가 박스 형태의 테두리를 갖도록 함
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(color: Colors.black),
+              color: Colors.yellow,
+            ),
+            width: width * 0.9,
+            height: height * 0.7,
+
+            child: Column(// 내부의 내용을 세로로 정렬함
+              children: <Widget>[// 여러개의 위젯을 넣을 수 있게 해줌
+                Padding(padding: EdgeInsets.only(top: height * 0.03)),// 여백을 조금 넣어줌
+                Container(// 문제의 제목 부분을 넣어줄 부분
+                  child: Text(
+                    '#' + widget.quiz.index.toString() + '. ' + widget.quiz.title,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: width * 0.06,
+                    ),
+                  ),
+                ),
+                Padding(padding: EdgeInsets.only(top: height * 0.03)),
+                Container(
+                  child: AutoSizeText(
+                    widget.quiz.question,
+                    textAlign: TextAlign.center,
+                    maxLines: 10,
+                    style: TextStyle(
+                      fontSize: width * 0.044,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Expanded(child: Container()),// 이후의 내용을 아래쪽부터 정렬되도록 함
+                ElevatedButton(
+                  child: Text(
+                    '정답 보기',
+                    style: TextStyle(
+                      fontSize: width * 0.06,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.amber[600],
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                    padding: EdgeInsets.fromLTRB(width * 0.22, height * 0.03, width *0.22, height * 0.03),
+                  ),
+                  onPressed: () {
+
+                  }
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
